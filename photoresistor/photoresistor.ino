e1a9675e7dedb5c132038881a6c8c4bb7facda44
Mychaelynn <mychaelynn@gmail.com>
2026-04-08 22:21:04 -0400

    test

--- photoresistor/photoresistor.ino
+++ photoresistor/photoresistor.ino
@@ -1,9 +1,37 @@
+
+int lightPen = A0;
+int lightVal;
+
+int red = 2;
+int green = 3;
+
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(lightPen,INPUT);
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+  Serial.begin(9600);
 
+  //as light gets darker --> resistence goes up --> current goes down --> voltage across 5k resistor will go down
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  lightVal = analogRead(lightPen);
+  Serial.print("Light value is: ");
+  Serial.println(lightVal);///
+
+  if (lightVal>600){//outside light is on
+    digitalWrite(red,HIGH);
+    digitalWrite(green,LOW);
+  } 
+  else{
+    digitalWrite(green,HIGH);
+    digitalWrite(red,LOW);
+  }
+  delay(250);
 
 }
